3879cf6e27a387569b14fa3e2d942d53871ddad7
Trevor Smith <4trevor@gmail.com>
2012-06-12 01:56:12 -0700

    First tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,11 +1,37 @@
 require 'spec_helper'
 
 describe "Static pages" do
+
   describe "Home page" do
-    it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get static_pages_home_path
-      response.status.should be(200)
+    it "should have the content 'Stop Smoking NOW!!!'" do
+      visit '/static_pages/home'
+      page.should have_content('Stop Smoking NOW!!!')
+    end
+    it "should have the right title" do 
+      visit '/static_pages/home'
+      page.should have_selector('title', text: "Habit Buster | Home")
+    end
+  end
+
+  describe "Help page" do
+    it "should have the content 'FAQ's'" do
+      visit '/static_pages/help'
+      page.should have_content('FAQ\'s')
+    end
+    it "should have the right title" do 
+      visit '/static_pages/help'
+      page.should have_selector('title', text: "Habit Buster | Help")
+    end
+  end
+
+  describe "About page" do
+    it "should have the content 'About Us'" do
+      visit '/static_pages/about'
+      page.should have_content('About Us')
+    end
+    it "should have the right title" do 
+      visit '/static_pages/about'
+      page.should have_selector('title', text: "Habit Buster | About Us")
     end
   end
 end
