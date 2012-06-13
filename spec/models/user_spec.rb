9d142a1af872104ec4aaf062bb860bd811e1dfca
Trevor Smith <4trevor@gmail.com>
2012-06-13 00:58:03 -0700

    End of Chapter 6

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -33,6 +33,7 @@ describe User do
     before { @user.email = '' }
     it { should_not be_valid }
   end
+
   describe "when email format is invalid" do
     it "should be invalid" do
       addresses = %w[user@foo,com user_at_foo.org example.user@foo.
