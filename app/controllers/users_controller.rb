cb424b0099a44ce862cbb5cf4f442465a4762814
Trevor Smith <4trevor@gmail.com>
2012-06-21 23:03:17 -0700

    Authorized complete w/o tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,14 @@
 
 class UsersController < ApplicationController
 
-def index
-  @users = User.all
-  
-end
+  before_filter :signed_in_user, except: :new 
+  #skip_before_filter :signed_in_user, only: :new
+  before_filter :correct_user, only: [:show, :edit, :update]
+
+
+  # def index
+  #   @users = User.all
+  # end
 
   def new
     @user = User.new
@@ -14,14 +18,45 @@ end
     @user = User.find(params[:id])
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Update successful"
+      # remember token gets reset when saved, so resign user in
+      sign_in @user
+      redirect_to @user
+    else
+      flash.now[:error] = "Try again"
+      render 'edit'
+    end
+  end
+
   def create
     @user = User.new(params[:user])
     if @user.save
+      sign_in @user
       flash[:success] = "Thank you for signing up"
       redirect_to @user
     else
       render 'new'
     end
-    
   end
+
+  private
+    def correct_user
+      @user = User.find(params[:id])
+      if !current_user?(@user)
+        sign_out
+        flash[:error] = "You can only edit your own content."
+        redirect_to root_path 
+      end
+    end
+
+    def signed_in_user
+       redirect_to signin_path, notice: "Please sign in." unless signed_in?
+    end 
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -18,6 +18,10 @@ module SessionsHelper
     @current_user = user    
   end
 
+  def current_user?(user)
+    user == User.find_by_remember_token(cookies[:remember_token])
+  end
+
   def current_user
     @current_user ||= User.find_by_remember_token(cookies[:remember_token])
   end
