b4f7c9ab9e7c083c488781a099b2e4bfed281ba3
Trevor Smith <4trevor@gmail.com>
2012-07-17 22:58:29 -0700

    added modal backdrop and created methods for it in site.js

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,6 +1,21 @@
-function removePreviousErrors()
-{
-      // remove validation errors from previous request
-    $('div.control-group').removeClass('error');
-    $('span.help-inline').remove();
-}
+
+  function removePreviousErrors()
+  {
+        // remove validation errors from previous request
+      $('div.control-group').removeClass('error');
+      $('span.help-inline').remove();
+  }
+
+  function showModalContent(content,header)
+  {
+    $('.modal-body').html(content);
+    $('#myModal').modal({keyboard:true, backdrop:true}).show();
+    $('div.modal-backdrop').css({'display':'block'});
+    $('#action_name').html(header);
+  }
+
+  function fadeModal()
+  {
+    $('div.modal').fadeOut('slow');
+    $('div.modal-backdrop').fadeOut('slow');
+  }
