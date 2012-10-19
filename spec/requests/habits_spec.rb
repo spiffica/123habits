b1c68dc4e68cb3c5391b0293e5147e99e6bedd75
Trevor Smith <4trevor@gmail.com>
2012-10-18 22:06:53 -0700

    wip habit-stats

--- spec/requests/habits_spec.rb
+++ spec/requests/habits_spec.rb
@@ -1,8 +1,10 @@
 require 'spec_helper'
 
 describe "Habits" do
+  let (:start_of_month) { Date.parse('1st July 2012') }
   context "with logged in user" do 
-  	before do
+    before do
+        Timecop.travel start_of_month
 	      @user = FactoryGirl.create(:user)
 	      @user2 = FactoryGirl.create(:user, email: "knob@here.com")
 	      @habit = FactoryGirl.create(:habit, user: @user)
@@ -18,6 +20,49 @@ describe "Habits" do
     it "doesn't allow access to other users habits" do
     	visit user_path(@user2)
     	page.should_not have_content(@habit2.statement)
+    end
+
+    context "with one habit started" do
+      before do
+        # @habit.status = "started"
+        # @habit.save
+        click_link "I will"
+        click_button "Start Habit Now!!"
+      end
+      describe "three bad days followed by 2 good days" do
+        before do
+          Timecop.travel 5.days
+          @habit.trackers.count.should == 21
+          3.times do |x|
+            @habit.trackers[x-1].success = false
+            @habit.trackers[x-1].save
+          end
+          2.times do |x|
+            @habit.trackers[x+2].success = true
+            @habit.trackers[x+2].save
+          end
+        end
+        it "shows stats box on habit#show page" do
+          @habit.trackers.success_days.count.should == 2
+          page.should have_css "aside#stats"
+        end
+        it "displays streak of 2" do
+          within(:css, "aside#stats") do
+            page.should have_css "#streak"
+            @habit.trackers.pending.count.should == 19
+            page.should have_content( "2 days")
+          end
+        end
+        it "displays percent success" do
+          pending
+        end
+        it "displays date started" do
+          pending
+        end
+        it "displays end date" do
+          pending
+        end
+      end
     end
   end
 end
