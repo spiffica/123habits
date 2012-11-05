7b66075111bffd2ce2e5928f650115cdfe47a1fd
Trevor Smith <4trevor@gmail.com>
2012-11-04 17:02:13 -0800

    added mobile mime and several .mobile.erb views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,21 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   include SessionsHelper
   before_filter :set_timezone
+  before_filter :prepare_mobile_request!
+ 
+
 
  
+  private
+	  def mobile_request?
+	  	request.user_agent =~ /Mobile|webOs/
+	  end
+	  helper_method :mobile_request?
+
+	 	def prepare_mobile_request!
+		 	if mobile_request?
+		 		request.format = :mobile
+		 	end
+		end
 
 end
