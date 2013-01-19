735cff76131012f848f169f8a9f5d991503d4f8c
Trevor Smith <4trevor@gmail.com>
2013-01-19 08:19:41 -0800

    isolated depency of TrackerManager in Habit

--- app/models/tracker_manager.rb
+++ app/models/tracker_manager.rb
@@ -1,31 +1,43 @@
 class TrackerManager
-
+  attr_reader :habit, :trackers
 	def initialize(habit)
 		@habit = habit
+    @trackers = habit.trackers
 	end
 
 	def create_initial_trackers
-		date = Time.zone.now.to_date
-    if @habit.completed_date && @habit.completed_date == date
-      date += 1.day
-    end
+		# date = Time.zone.now.to_date
+  #   #remove @habit.completed_date
+  #   if @habit.completed_date && @habit.completed_date == date
+  #     date += 1.day
+  #   end
+    date = set_start_date
     (Habit::LENGTH).times do |x|
       state = x == 0? "current" : "pending"
-      @habit.trackers.create(day:date, outcome: state)
+      trackers.create(day:date, outcome: state)
       date += 1.day
     end
   end
 
   def delete_trackers
-  	@habit.trackers.delete_all
+  	trackers.delete_all
   end
 
   def add_penalty_trackers 
-  	date = @habit.trackers.last.day
-    @habit.penalty.times do |n|
+  	date = trackers.last.day
+    habit.penalty.times do |n|
       date += 1.day
-      @habit.trackers.create(day:date)
+      trackers.create(day:date)
     end
   end
+
+  private
+    def set_start_date
+      date = Time.zone.now.to_date
+      if habit.completed_date == date
+        date += 1.day
+      end
+      date
+    end
 
 end
